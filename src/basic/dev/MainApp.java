7c7838280cab3078bdd10ce07c544dce17ff0c64
Truong Ngoc Trinh <bachtran1p@gmail.com>
2021-10-23 20:58:25 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,41 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("[...........................] ");
+        System.out.println("Thong tin mot sinh vien:");
+        String ten ="Truong Ngoc Trinh";
+        System.out.println("Ten: " + ten);
+        String maSinhVien = "2160717";
+        System.out.println("Ma Sinh Vien: " + maSinhVien);
+        String cmnd = "19787978";
+        System.out.println("Chung Minh Nhan Dan: " + cmnd);
+        String tenLop = "K21CDTH";
+        System.out.println("Ten Lop: " + tenLop);
+        float diemTrungBinh = 6.75f;
+        System.out.println("Diem Trung Binh: " + diemTrungBinh);
+        System.out.println("<---------------------------->");
+        System.out.println("Thong Tin Cong Dan:");
+        String tenCongDan = "Truong Ngoc Trinh";
+        System.out.println("Ten Cong Dan: "+ tenCongDan);
+        String diaChi = "Phu Vang";
+        System.out.println("Dia Chi: " + diaChi);
+        String dienThoai = "0389159803";
+        System.out.println("So Dien Thoai: " + dienThoai);
+        String heSoLuong = "9999999";
+        System.out.println("He So Luong: " + heSoLuong);
+        System.out.println("<---------------------------->");
+        System.out.println("Thong Tin Mot Chiec Xe Car: ");
+        String tenXe = "Vinfast";
+        System.out.println("Ten Xe: " + tenXe);
+        String hangXe = "VIETNAM";
+        System.out.println("Hang Xe: " + hangXe);
+        String giaXe = "1.358.554.000 VND";
+        System.out.println("Gia Xe: " + giaXe);
+        String mauSon = "Do Candy";
+        System.out.println("Mau Son Xe: " + mauSon);
+
+	}
+
+}
